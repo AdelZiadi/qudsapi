04d14a774e3455ea1a1f75abf95bf536e58d6241
Hamza <hamza@nmcpye.org>
2024-05-25 22:07:07 +0300

    add uid generator

--- src/main/java/org/nmcpye/datarun/service/impl/AssignmentServiceImplCustom.java
+++ src/main/java/org/nmcpye/datarun/service/impl/AssignmentServiceImplCustom.java
@@ -4,14 +4,12 @@ import org.nmcpye.datarun.domain.Assignment;
 import org.nmcpye.datarun.repository.AssignmentRepositoryExt;
 import org.nmcpye.datarun.security.SecurityUtils;
 import org.nmcpye.datarun.service.AssignmentServiceExt;
+import org.nmcpye.datarun.utils.CodeGenerator;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
 
 @Service
 @Primary
@@ -29,5 +27,13 @@ public class AssignmentServiceImplExt extends AssignmentServiceImpl implements A
         final String userLogin = SecurityUtils.getCurrentUserLogin()
             .orElseThrow(() -> new IllegalStateException("User not logged in"));
         return assignmentRepositoryExt.findByCurrentUser(userLogin, pageable);
+    }
+
+    @Override
+    public Assignment save(Assignment assignment) {
+        if (assignment.getUid() == null || assignment.getUid().isEmpty()) {
+            assignment.setUid(CodeGenerator.generateUid());
+        }
+        return super.save(assignment);
     }
 }
